4250663dfb6bf00dde54a626851cf81c7ddd4500
saint6839 <saint6839@gmail.com>
2022-07-05 23:13:59 +0900

    fix: 데이터베이스 연동 오류 해결

--- handtalk/src/main/java/handtalkproject/domain/entity/User.java
+++ handtalk/src/main/java/handtalkproject/domain/entity/User.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
     private String email;
