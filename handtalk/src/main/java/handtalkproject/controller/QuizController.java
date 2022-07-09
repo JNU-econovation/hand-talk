3b2edcfe2a72a2ea7ef84155dae29efd48b6a810
saint6839 <saint6839@gmail.com>
2022-07-09 11:16:43 +0900

    feat: 퀴즈 문제 출제 및 오답 저장 기능 구현

--- handtalk/src/main/java/handtalkproject/controller/QuizController.java
+++ handtalk/src/main/java/handtalkproject/controller/QuizController.java
@@ -1,20 +1,47 @@
 package handtalkproject.controller;
 
+import handtalkproject.domain.dto.QuizMultipleChoiceDto;
+import handtalkproject.domain.dto.WrongQuizHandTalkDto;
 import handtalkproject.domain.entity.HandTalk;
+import handtalkproject.domain.entity.User;
+import handtalkproject.domain.entity.WrongQuizHandTalk;
+import handtalkproject.exception.NoAuthenticationException;
 import handtalkproject.service.QuizService;
+import handtalkproject.utils.UserSessionUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 
 @RestController
 @RequiredArgsConstructor
 public class QuizController {
+    private static final String NO_AUTHENTICATION_MESSAGE = "로그인이 필요한 서비스 입니다.";
+
     private final QuizService quizService;
+    private final HttpSession session;
 
     @GetMapping("/quiz/{day}")
-    public HandTalk showMultipleChoice(@PathVariable int day) {
-        return quizService.showMultipleChoice(day);
+    public QuizMultipleChoiceDto showMultipleChoice(@PathVariable int day) {
+        if(getLoginedUser() != null) {
+            return quizService.showQuizMultipleChoices(day);
+        }
+        throw new NoAuthenticationException(NO_AUTHENTICATION_MESSAGE);
+    }
+
+    @PostMapping("/quiz/wrong")
+    public void saveWrongQuizHandTalk(WrongQuizHandTalkDto wrongQuizHandTalkDto) {
+        if(getLoginedUser() != null) {
+            quizService.saveWrongQuizHandTalk(getLoginedUser(), wrongQuizHandTalkDto.toEntity());
+        }
+        throw new NoAuthenticationException(NO_AUTHENTICATION_MESSAGE);
+    }
+
+    public User getLoginedUser() {
+        return (User)session.getAttribute(UserSessionUtils.USER_SESSION_KEY);
     }
 }
--- handtalk/src/main/java/handtalkproject/service/QuizService.java
+++ handtalk/src/main/java/handtalkproject/service/QuizService.java
@@ -1,7 +1,11 @@
 package handtalkproject.service;
 
+import handtalkproject.domain.dto.QuizMultipleChoiceDto;
 import handtalkproject.domain.entity.HandTalk;
+import handtalkproject.domain.entity.User;
+import handtalkproject.domain.entity.WrongQuizHandTalk;
 import handtalkproject.repository.HandTalkRepository;
+import handtalkproject.repository.WrongQuizHandTalkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +16,38 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class QuizService {
-    private static final int FIRST_INDEX = 0;
+    private static final int ANSWER_INDEX = 0;
+    private static final int WRONG_INDEX= 1;
+    private static final int WRONG_INDEX2 = 2;
+
 
     private final HandTalkRepository handTalkRepository;
 
-    public HandTalk showMultipleChoice(int day) {
+    private final WrongQuizHandTalkRepository wrongQuizHandTalkRepository;
+
+    public QuizMultipleChoiceDto showQuizMultipleChoices(int day) {
         List<HandTalk> datas = handTalkRepository.findAll()
                                                    .stream()
                                                    .filter(h -> h.isDay(day))
                                                    .collect(Collectors.toList());
         Collections.shuffle(datas);
-        return datas.get(FIRST_INDEX);
+
+        // 정답에 해당하는 동영상과 단어 뜻 0번 인덱스에 저장
+        QuizMultipleChoiceDto quizMultiChoiceDto = new QuizMultipleChoiceDto(datas.get(ANSWER_INDEX));
+
+        // 나머지 오답에 해당하는 객관식 선지에 들어갈 단어들을 QuizMultipleChoiceDto 객체에 저장
+        for (int i = WRONG_INDEX; i <= WRONG_INDEX2; i++) {
+            quizMultiChoiceDto.addWrongMulitipleChoice(datas.get(i).getHandtalkValue());
+        }
+
+        return quizMultiChoiceDto;
+    }
+
+    public void saveWrongQuizHandTalk(User loginedUser, HandTalk handTalk) {
+        WrongQuizHandTalk wrongQuizHandTalk = WrongQuizHandTalk.builder()
+                                                               .user(loginedUser)
+                                                               .handTalk(handTalk)
+                                                               .build();
+        wrongQuizHandTalkRepository.save(wrongQuizHandTalk);
     }
 }
