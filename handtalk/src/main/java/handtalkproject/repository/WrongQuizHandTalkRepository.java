776c5efc1087a374034598d5f6b08728bd5b7d24
saint6839 <saint6839@gmail.com>
2022-07-09 16:50:53 +0900

    feat: 오답 저장을 위한 저장소 생성

---
+++ handtalk/src/main/java/handtalkproject/repository/WrongQuizHandTalkRepository.java
@@ -0,0 +1,7 @@
+package handtalkproject.repository;
+
+import handtalkproject.domain.entity.WrongQuizHandTalk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WrongQuizHandTalkRepository extends JpaRepository<WrongQuizHandTalk, Long> {
+}
