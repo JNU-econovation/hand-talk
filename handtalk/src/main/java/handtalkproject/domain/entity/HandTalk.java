61e1591cf4edf04214596e61adc32b26e4a99ead
saint6839 <saint6839@gmail.com>
2022-07-09 11:18:04 +0900

    feat: 수어와 오답엔티티가 1:N 관계를 갖도록 필드 추가

--- handtalk/src/main/java/handtalkproject/domain/entity/HandTalk.java
+++ handtalk/src/main/java/handtalkproject/domain/entity/HandTalk.java
@@ -1,9 +1,11 @@
 package handtalkproject.domain.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,9 +20,27 @@ public class HandTalk {
     @JoinColumn(name = "day_id")
     private Day day;
 
+    @OneToMany(mappedBy = "handTalk")
+    private List<WrongQuizHandTalk> wrongQuizHandTalk;
+
     private String videoUrl;
 
     private String handtalkValue;
+
+    @Builder
+    public HandTalk(Day day, List<WrongQuizHandTalk> wrongQuizHandTalk, String videoUrl, String handtalkValue) {
+        this.day = day;
+        this.wrongQuizHandTalk = wrongQuizHandTalk;
+        this.videoUrl = videoUrl;
+        this.handtalkValue = handtalkValue;
+    }
+
+    @Builder
+
+
+    public HandTalk() {
+
+    }
 
     public boolean isDay(int day) {
         return this.day.getDay() == day;
